bcc53837d696f678eea38ba8b19adc59d1f605f9
blnche <peltierblanche@gmail.com>
2023-07-25 10:58:33 +0200

    added go up link in main and moved themebtn in main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 window.addEventListener("DOMContentLoaded", function () {
+
+    /*  =======================
+        THEME BUTTON
+        ======================= */
+
     let toggleThemeBtn = document.getElementById("themeBtn");
+    // let themeBtnSymbol = document.querySelector('#themeBtn i');
+    // console.log(themeBtnSymbol);
 
     toggleThemeBtn.addEventListener("click", function () {
         let currentTheme = document.documentElement.getAttribute('data-theme');
@@ -7,5 +14,16 @@ window.addEventListener("DOMContentLoaded", function () {
         let newTheme = currentTheme === 'dark' ? 'light' : currentTheme === 'light' ? 'dark' : currentTheme;
 
         document.documentElement.setAttribute('data-theme', newTheme);
-    })
+        
+        // if (currentTheme === 'dark')
+        // {
+        //     themeBtnSymbol.classList.remove('fa-sharp fa-solid fa-moon');
+        //     themeBtnSymbol.classList.add('fa-solid fa-sun-bright');
+        // }
+        // else if (currentTheme === 'light')
+        // {
+        //     themeBtnSymbol.classList.remove('fa-solid fa-sun-bright');
+        //     themeBtnSymbol.classList.add('fa-sharp fa-solid fa-moon');
+        // }
+    });
 })
