ec3ff2cfac8e12d1aa626b93c1681a78de309ee4
blnche <peltierblanche@gmail.com>
2023-09-29 22:23:33 +0200

    worked on style, public layout & header with hamburger nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
 // IMPORT
 import {validateUserLoginForm} from '/assets/js/functions/user-validate-forms.js';
 import {validateUserRegisterForm} from '/assets/js/functions/user-validate-forms.js';
+import {burgerMenu, foldingMenu} from '/assets/js/functions/menus.js';
 // END IMPORT
 
 window.addEventListener("DOMContentLoaded", function () {
+    burgerMenu();
+    foldingMenu();
+
     console.log(window.location.pathname);
     let path = window.location.pathname;
 
