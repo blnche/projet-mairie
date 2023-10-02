7c2c825503b4f15a2dac3e87ee5573a615094a8b
blnche <peltierblanche@gmail.com>
2023-10-02 19:54:14 +0000

    weeks search added on admin

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,13 @@ import {validateUserLoginForm} from '/projet-final/projet-mairie/assets/js/funct
 import {validateUserRegisterForm} from '/projet-final/projet-mairie/assets/js/functions/user-validate-forms.js';
 import {burgerMenu, foldingMenu} from '/projet-final/projet-mairie/assets/js/functions/menus.js';
 import {lightDarkMode} from '/projet-final/projet-mairie/assets/js/functions/theme-mode.js';
+import {search} from '/projet-final/projet-mairie/assets/js/functions/search.js';
 // END IMPORT
 
 window.addEventListener("DOMContentLoaded", function () {
     burgerMenu();
     foldingMenu();
+    search();
 
     console.log(window.location.pathname);
     let path = window.location.pathname;
